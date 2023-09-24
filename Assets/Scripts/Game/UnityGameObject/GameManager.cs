a434d63b3601e89a8c19164ea7379dba1f02142d
noname74jp <83318960+noname74jp@users.noreply.github.com>
2023-09-24 16:09:28 +0900

    内部処理をタイトルとインゲームに分割

--- Assets/Scripts/Game/UnityGameObject/GameManager.cs
+++ Assets/Scripts/Game/UnityGameObject/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Game.Logic;
 using Game.UnityGameObject.Char;
 using UnityEngine;
@@ -10,12 +13,77 @@ namespace Game.UnityGameObject
     /// </summary>
     public class GameManager : MonoBehaviour
     {
+        #region classes
+
+        /// <summary>
+        /// 入力アクションデータ。
+        /// </summary>
+        [Serializable]
+        private class InputActionData
+        {
+            #region variables
+
+            /// <summary>
+            /// 決定。
+            /// </summary>
+            [SerializeField] public InputAction decide;
+
+            /// <summary>
+            /// リトライ。
+            /// </summary>
+            [SerializeField] public InputAction retry;
+
+            #endregion
+
+            #region methods
+
+            /// <summary>
+            /// 有効にする。
+            /// </summary>
+            public void Enable()
+            {
+                decide.Enable();
+                retry.Enable();
+            }
+
+            /// <summary>
+            /// 無効にする。
+            /// </summary>
+            public void Disable()
+            {
+                decide.Disable();
+                retry.Disable();
+            }
+
+            /// <summary>
+            /// 決定ボタンが押されているか。
+            /// </summary>
+            /// <returns></returns>
+            public bool IsDecideKeyPressed()
+            {
+                return decide.ReadValue<float>() >= 0.5f;
+            }
+
+            /// <summary>
+            /// リトライボタンが押されているか。
+            /// </summary>
+            /// <returns></returns>
+            public bool IsRetryKeyPressed()
+            {
+                return retry.ReadValue<float>() >= 0.5f;
+            }
+
+            #endregion
+        }
+
+        #endregion
+
         #region variables
 
         /// <summary>
-        /// 入力アクション。
+        /// 入力アクションデータ。
         /// </summary>
-        [SerializeField] private InputAction inputAction;
+        [SerializeField] private InputActionData inputActionData;
 
         /// <summary>
         /// プレイヤーのオブジェクト。
@@ -32,6 +100,11 @@ namespace Game.UnityGameObject
         /// </summary>
         private double _timer;
 
+        /// <summary>
+        /// キャンセルトークン。
+        /// </summary>
+        private CancellationTokenSource _cts;
+
         #endregion
 
         #region methods
@@ -41,8 +114,7 @@ namespace Game.UnityGameObject
         /// </summary>
         private void Awake()
         {
-            inputAction?.Enable();
-            Initialize();
+            StartGameLoop();
         }
 
         /// <summary>
@@ -50,7 +122,7 @@ namespace Game.UnityGameObject
         /// </summary>
         private void OnEnable()
         {
-            inputAction?.Enable();
+            inputActionData.Enable();
         }
 
         /// <summary>
@@ -58,27 +130,41 @@ namespace Game.UnityGameObject
         /// </summary>
         private void OnDisable()
         {
-            inputAction?.Disable();
+            inputActionData.Disable();
         }
 
         /// <summary>
-        /// Unityイベント関数Update。
+        /// Unityイベント関数OnDestroy。
         /// </summary>
-        public void Update()
+        private void OnDestroy()
         {
-            // 入力管理
-            var isKeyPressed = inputAction != null && inputAction.ReadValue<float>() >= 0.5f;
+            StopGameLoop();
+        }
 
-            // ロジックの更新
-            _timer += Time.deltaTime;
-            while (_timer >= Defines.SecondsPerFrame)
-            {
-                _timer -= Defines.SecondsPerFrame;
-                _playerLogic.UpdateStatus(isKeyPressed);
-            }
+        /// <summary>
+        /// Unityイベント関数OnApplicationQuit。
+        /// </summary>
+        private void OnApplicationQuit()
+        {
+            StopGameLoop();
+        }
 
-            // Unity上の更新
-            player.UpdateStatus(_playerLogic);
+        /// <summary>
+        /// ゲームループを開始する。
+        /// </summary>
+        private void StartGameLoop()
+        {
+            _cts = new CancellationTokenSource();
+            UpdateTitle(_cts.Token).Forget();
+        }
+
+        /// <summary>
+        /// ゲームループを停止する。
+        /// </summary>
+        private void StopGameLoop()
+        {
+            _cts?.Cancel();
+            _cts = null;
         }
 
         /// <summary>
@@ -90,6 +176,77 @@ namespace Game.UnityGameObject
             _playerLogic = new PlayerLogic();
             _playerLogic.Initialize();
             player.UpdateStatus(_playerLogic);
+        }
+
+        /// <summary>
+        /// タイトルを更新する。
+        /// </summary>
+        /// <param name="token">キャンセルトークン。</param>
+        private async UniTaskVoid UpdateTitle(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            // 初期化
+            Initialize();
+
+            // メインループ
+            while (true)
+            {
+                // 入力管理
+                var isDecideKeyPressed = inputActionData.IsDecideKeyPressed();
+
+                // ゲーム開始
+                if (inputActionData.IsDecideKeyPressed())
+                {
+                    break;
+                }
+
+                // 次のフレームへ
+                await UniTask.NextFrame(token);
+//                token.ThrowIfCancellationRequested();
+            }
+
+            // タイトル終了処理
+            UpdateGame(token).Forget();
+        }
+
+        /// <summary>
+        /// ゲームを更新する。
+        /// </summary>
+        /// <param name="token">キャンセルトークン。</param>
+        private async UniTaskVoid UpdateGame(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            // メインループ
+            while (true)
+            {
+                // 入力管理
+                var isDecideKeyPressed = inputActionData.IsDecideKeyPressed();
+                var isRetryKeyPressed = inputActionData.IsRetryKeyPressed();
+
+                // リトライ
+                if (isRetryKeyPressed)
+                {
+                    UpdateTitle(token).Forget();
+                    break;
+                }
+
+                // ロジックの更新
+                _timer += Time.deltaTime;
+                while (_timer >= Defines.SecondsPerFrame)
+                {
+                    _timer -= Defines.SecondsPerFrame;
+                    _playerLogic.UpdateStatus(isDecideKeyPressed);
+                }
+
+                // Unity上の更新
+                player.UpdateStatus(_playerLogic);
+
+                // 次のフレームへ
+                await UniTask.NextFrame(token);
+//                token.ThrowIfCancellationRequested();
+            }
         }
 
         #endregion
